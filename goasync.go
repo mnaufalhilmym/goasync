8b8f2d268fa91a6014b063a53b16e96b8d21f6cc
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2024-09-25 10:02:00 +0700

    add doc

--- goasync.go
+++ goasync.go
@@ -12,10 +12,19 @@ type JoinHandle[T any] struct {
 	cancel context.CancelFunc
 }
 
+// Abort the task associated with the handle.
+//
+// Awaiting a cancelled task might complete as usual if the task was
+// already completed at the time it was cancelled, but most likely it
+// will fail with a `context canceled`.
 func (h *JoinHandle[T]) Abort() {
 	h.cancel()
 }
 
+// Suspend execution until the result of a `JoinHandle` is ready.
+//
+// `.Await`ing a future will suspend the current function's execution
+// until the executor has run the future to completion.
 func (h *JoinHandle[T]) Await(ctx context.Context) (T, error) {
 	for {
 		if *h.result != nil {
@@ -37,10 +46,31 @@ func (h *JoinHandle[T]) Await(ctx context.Context) (T, error) {
 	}
 }
 
+// Checks if the task associated with this `JoinHandle` has finished.
+//
+// Please note that this method can return `false` even if `Abort` has been
+// called on the task. This is because the cancellation process may take
+// some time, and this method does not return `true` until it has
+// completed.
 func (h *JoinHandle[T]) IsFinished() bool {
 	return (*h.result) != nil
 }
 
+// Spawns a new asynchronous task, returning a
+// `JoinHandle[T any]` for it.
+//
+// The provided future will start running in the background immediately
+// when `Spawn` is called, even if you don't await the returned
+// `JoinHandle`.
+//
+// Spawning a task enables the task to execute concurrently to other tasks. The
+// spawned task may execute on the current thread, or it may be sent to a
+// different thread to be executed. The specifics depend on how the Go runtime
+// schedules it.
+//
+// There is no guarantee that a spawned task will execute to completion.
+// When a runtime is shutdown, all outstanding tasks are dropped,
+// regardless of the lifecycle of that task.
 func Spawn[T any](fn func(context.Context) (T, error)) JoinHandle[T] {
 	ctx, cancel := context.WithCancel(context.Background())
 	doneCh := make(chan byte, 1)
@@ -65,6 +95,11 @@ func Spawn[T any](fn func(context.Context) (T, error)) JoinHandle[T] {
 	}
 }
 
+// Waits on multiple concurrent branches, returning when all branches
+// complete with `success` value or on the first `error`.
+//
+// The `TryJoin` function takes a list of `JoinHandle` and evaluates them
+// concurrently.`.
 func TryJoin[T any](ctx context.Context, handles ...JoinHandle[T]) ([]T, error) {
 	countHandles := len(handles)
 
